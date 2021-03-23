5175fbe0253cb86911be5a9d11b6e7883d0055f8
Rojitha <rojitha.kamal@gmail.com>
2021-03-23 23:07:37 +0400

    added ifthenelse

--- custom/functions.py
+++ custom/functions.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 # Specify the URL to your package here.
 # This URL must be accessible via pip install
 #PACKAGE_URL = 'git+https://github.com/madendorff/functions@'
-PACKAGE_URL = 'git+https://github.com/kkbankol-ibm/watson-analytics-dashboard@'
+PACKAGE_URL = 'git+https://github.com/rojithakamal/rkamal'
 
 class MaximoAssetHTTP(BasePreload):
     '''
@@ -487,3 +487,47 @@ class MaximoAssetHTTP(BasePreload):
         outputs=[]
         outputs.append(ui.UIStatusFlag(name='output_item'))
         return (inputs, outputs)
+class customIfThenElse(BaseTransformer):
+    """
+    Set the value of the output_item based on a conditional expression.
+    When the conditional expression returns a True value, return the value of the true_expression.
+    Example:
+    conditional_expression: df['x1'] > 5 * df['x2']
+    true expression: df['x2'] * 5
+    false expression: 0
+    """
+
+    def __init__(self, conditional_expression, true_expression, false_expression, output_item=None):
+        super().__init__()
+        self.conditional_expression = self.parse_expression(conditional_expression)
+        self.true_expression = self.parse_expression(true_expression)
+        self.false_expression = self.parse_expression(false_expression)
+        if output_item is None:
+            self.output_item = 'output_item'
+        else:
+            self.output_item = output_item
+
+    def execute(self, df):
+        c = self._entity_type.get_attributes_dict()
+        df = df.copy()
+        df[self.output_item] = np.where(eval(self.conditional_expression), eval(self.true_expression),
+                                        eval(self.false_expression))
+        return df
+
+    @classmethod
+    def build_ui(cls):
+        # define arguments that behave as function inputs
+        inputs = []
+        inputs.append(UIExpression(name='conditional_expression', description="expression that returns a True/False value, \
+                                                eg. if df['temp']>50 then df['temp'] else None"))
+        inputs.append(UIExpression(name='true_expression', description="expression when true, eg. df['temp']"))
+        inputs.append(UIExpression(name='false_expression', description='expression when false, eg. None'))
+        # define arguments that behave as function outputs
+        outputs = []
+        outputs.append(UIFunctionOutSingle(name='output_item', datatype=bool, description='Dummy function output'))
+
+        return (inputs, outputs)
+
+    def get_input_items(self):
+        items = self.get_expression_items([self.conditional_expression, self.true_expression, self.false_expression])
+        return items
